441026af2b0ef8ded189f02588c09fe7554edf92
Mohammed Haitham <mohamedhaithame320@gmail.com>
2025-05-04 16:57:56 +0200

    init the server and API working

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-// this is the server
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/',(req,res) => {
+  res.send("API WORKING")
+}
+)
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
+})
